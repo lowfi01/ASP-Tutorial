6af8fa7af7afb1e7ebf06e50c02ad7dbd470a465
JAMES HERRERO <100859697@student.swin.edu.au>
2020-02-16 16:25:23 +1100

    start lecture15

--- ASP-Tutorial/App_Start/RouteConfig.cs
+++ ASP-Tutorial/App_Start/RouteConfig.cs
@@ -13,11 +13,22 @@ namespace ASP_Tutorial
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();  // better implementation, requires [Route] in controller
+
+            routes.MapRoute(
+                name: "MoviesByReleaseDate",
+                url: "movies/released/{year}/{month}",
+                defaults: new { Controller = "Movie", action = "ByReleaseDate" },
+                new { year = @"2015|2016", month = @"\d{2}"}
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
         }
     }
 }
--- ASP-Tutorial/Controllers/MovieController.cs
+++ ASP-Tutorial/Controllers/MovieController.cs
@@ -13,7 +13,40 @@ namespace ASP_Tutorial.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Life" };
-            return View(movie);
+
+            ViewData["Movie"] = movie;
+
+            return View();
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+
+            // Using default params 
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+        [Route("Movie/released/{year}/{month:regex(\\d{2})}")]
+        public ActionResult ReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
+
 }
